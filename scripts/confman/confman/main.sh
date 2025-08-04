ecc8e4f5420786b557081c06684ddf61728cc3a1
IamShinn <shin.dean.fwd@gmail.com>
2025-08-04 13:01:05 +0200

    automated bash script

---
+++ scripts/confman/confman/main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##packages
+sudo bash $(dirname "$0")/packages.sh
+git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+
+
+#yay operations
+yay -S --noconfirm --needed zen-browser-bin
+yay -S --noconfirm --needed tofi 
+yay -S --noconfirm --needed tauon-music-box 
+
+##dotfiles
+git clone https://github.com/IamShinn/dotfiles
+cd $HOME/dotfiles/
+sudo stow --adopt .
+
+yay -Yc
---
+++ scripts/confman/confman/packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#pacman instalation
+
+yes | sudo pacman -Syu
+yes | sudo pacman -S --needed ghostty syncthing waybar ttc-iosevka imv mpv pavucontrol fastfetch hyprpolkitagent neovim grc hyprpaper cowfortune nwg-look stow ttf-nerd-fonts-symbols
+
+#installing yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cd ..
+rm -rf yay
+
+#thunar
+sudo pacman -S --needed thunar fileroller thunar-archive-plugin thunar-media-tags-plugin ffmpegthumbnailer tumbler gvfs
