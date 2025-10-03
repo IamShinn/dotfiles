fb9d80328b210559db23ea51a36b829edce41830
IamShinn <shin.dean.fwd@gmail.com>
2025-10-03 17:01:03 +0200

    package update

--- scripts/confman/packages.sh
+++ scripts/confman/packages.sh
@@ -2,7 +2,7 @@
 #pacman instalation
 
 yes | sudo pacman -Syu
-yes | sudo pacman -S --needed ghostty syncthing waybar ttc-iosevka imv mpv pavucontrol fastfetch hyprpolkitagent neovim grc hyprpaper cowfortune nwg-look stow ttf-nerd-fonts-symbols xdg-desktop-portal xdg-desktop-portal-wlr grim hyprshot
+yes | sudo pacman -S --needed ghostty syncthing waybar ttc-iosevka imv mpv pavucontrol alsa-utils fastfetch hyprpolkitagent neovim grc hyprpaper cowfortune nwg-look stow ttf-nerd-fonts-symbols xdg-desktop-portal xdg-desktop-portal-wlr grim hyprshot 
 
 #installing yay
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
