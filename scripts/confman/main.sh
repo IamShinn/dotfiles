059b15d5cf99b3f634b4ea039799e3ca97886907
IamShinn <shin.dean.fwd@gmail.com>
2025-10-25 19:32:15 +0200

    Fixed boot config setups

--- scripts/confman/main.sh
+++ scripts/confman/main.sh
@@ -1,27 +1,29 @@
-#!/bin/bash
-##packages
+# !/bin/bash
+#packages
 sudo bash $(dirname "$0")/packages.sh
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 
 #yay operations
-yay -S --noconfirm --needed zen-browser-bin
-yay -S --noconfirm --needed tofi 
-yay -S --noconfirm --needed tauon-music-box 
-yay -S --noconfirm --needed dtach-ng-bin
+# yay -S --noconfirm --needed zen-browser-bin
+# yay -S --noconfirm --needed tofi 
+# yay -S --noconfirm --needed tauon-music-box 
+# yay -S --noconfirm --needed dtach-ng-bin
 
 ##dotfiles
-git clone https://github.com/IamShinn/dotfiles
+# git clone https://github.com/IamShinn/dotfiles
 cd dotfiles
 sudo stow -S *
+cd ..
 
 ##dotfiles outside dotfiles
-sudo cp -r ./boot/hyprland-mac-style/ /usr/share/plymouth/themes/
+sudo cp -r $(dirname "$0")/boot/hyprland-mac-style/ /usr/share/plymouth/themes/
 sudo plymouth-set-default-theme -R hyprland-mac-style 
 sudo mkinitcpio -p linux
 
 ##greetd setup
-rm -rf /etc/greetd/ 
-cp -r ./boot/greetd/ /etc/
+sudo rm -rf /etc/greetd/ 
+sudo mkdir /etc/greetd/
+sudo cp -r "$(dirname "$0")/boot/greetd"/* /etc/greetd/
 
 yay -Yc --noconfirm
--- scripts/confman/packages.sh
+++ scripts/confman/packages.sh
@@ -2,7 +2,9 @@
 #pacman instalation
 
 yes | sudo pacman -Syu
-yes | sudo pacman -S --needed ghostty syncthing waybar ttc-iosevka imv mpv pavucontrol alsa-utils fastfetch hyprpolkitagent neovim grc hyprpaper cowfortune nwg-look stow ttf-nerd-fonts-symbols xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-wlr grim hyprshot noto-fonts noto-fonts-cjk noto-fonts-cjk noto-fonts-extra iwd impala bash-completion plymouth ly greetd greetd-regreet
+mapfile -t packages < <(grep -v '^#' "$(dirname "$0")/packages-list" | grep -v '^$')
+sudo pacman -S --noconfirm --needed "${packages[@]}"
+# yes | sudo pacman -S --needed ghostty syncthing waybar ttc-iosevka imv mpv pavucontrol alsa-utils fastfetch hyprpolkitagent neovim grc hyprpaper cowfortune nwg-look stow ttf-nerd-fonts-symbols xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-wlr grim hyprshot noto-fonts noto-fonts-cjk noto-fonts-cjk noto-fonts-extra iwd impala bash-completion plymouth ly greetd greetd-regreet
 
 #installing yay
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
