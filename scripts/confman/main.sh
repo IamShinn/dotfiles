6da967af45a812b38bc1c0209263423d11395875
IamShinn <omg.minegod@gmail.com>
2026-03-06 12:52:41 +0100

    changed cursor to the posy cursor

--- scripts/confman/main.sh
+++ scripts/confman/main.sh
@@ -30,6 +30,7 @@ sudo mkinitcpio -p linux -c custom-hooks.conf
 gsettings set org.gnome.desktop.interface gtk-theme 'oomox-me'
 gsettings set org.gnome.desktop.interface icon-theme 'oomox-Gigavolt' 
 gsettings get org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font 11'
+gsettings set org.gnome.desktop.interface cursor-theme 'Posy Cursor Black'
 
 ##greetd setup
 sudo rm -rf /etc/greetd/ 
